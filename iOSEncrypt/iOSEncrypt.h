670c8fd741990df73d45164cb8efe077477cb601
Yuhan Liu <yliu1021@outlook.com>
2021-11-03 21:16:19 -0700

    Initial Commit

---
+++ iOSEncrypt/iOSEncrypt.h
@@ -0,0 +1,18 @@
+//
+//  iOSEncrypt.h
+//  iOSEncrypt
+//
+//  Created by Yuhan Liu on 11/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSEncrypt.
+FOUNDATION_EXPORT double iOSEncryptVersionNumber;
+
+//! Project version string for iOSEncrypt.
+FOUNDATION_EXPORT const unsigned char iOSEncryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSEncrypt/PublicHeader.h>
+
+
